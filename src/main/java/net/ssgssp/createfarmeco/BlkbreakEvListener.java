5613ca93b30b105bd18ce66c4bc307961e5c987a
mrsongbyssgs <3085169511@qq.com>
2024-03-06 22:41:00 +0800

    1.3 Update Fix the capability problems when Mekanism is installed in server.

--- src/main/java/net/ssgssp/createfarmeco/BlkbreakEvListener.java
+++ src/main/java/net/ssgssp/createfarmeco/BlkbreakEvListener.java
@@ -1,6 +1,7 @@
 package net.ssgssp.createfarmeco;
 
 
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -12,7 +13,8 @@ import org.bukkit.plugin.Plugin;
 import java.util.Objects;
 
 public class BlkbreakEvListener implements Listener {
-    Plugin plug1 =  CreateFarmECO.getPlugin(CreateFarmECO.class);
+    Plugin plug1 = CreateFarmECO.getPlugin(CreateFarmECO.class);
+
     @EventHandler
     public void BlkbreakEv(BlockBreakEvent bee) {
         plug1.reloadConfig();
@@ -22,10 +24,13 @@ public class BlkbreakEvListener implements Listener {
         int min1 = plug1.getConfig().getInt("BlockbreakMin");
         boolean bool1 = plug1.getConfig().getBoolean("isPlayerOn");
         Block block1 = bee.getBlock();
-        if(bee.isDropItems()){
-            if (block1.getType().name().toLowerCase().endsWith("ore")) {
-                if (!(player1.getInventory().getItemInMainHand().containsEnchantment(Enchantment.SILK_TOUCH))) {
-                    net.ssgssp.createfarmeco.api.ECOapi.MessageSender(player1, max1, min1, string1, bool1);
+        Location location1 = player1.getLocation();
+        if (!location1.getBlock().getType().name().toLowerCase().endsWith("digital_miner")) {
+            if (bee.isDropItems()) {
+                if (block1.getType().name().toLowerCase().endsWith("ore")) {
+                    if (!(player1.getInventory().getItemInMainHand().containsEnchantment(Enchantment.SILK_TOUCH))) {
+                        net.ssgssp.createfarmeco.api.ECOapi.MessageSender(player1, max1, min1, string1, bool1);
+                    }
                 }
             }
         }
